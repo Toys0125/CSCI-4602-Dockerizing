4e7905f85ed0e5ed0c45363a3ddafaa02d2d4904
Toys0125 <marcus.g.stange@gmail.com>
2021-12-01 18:58:53 -0600

    Working Enhancements

--- Client/src/client.ts
+++ Client/src/client.ts
@@ -16,6 +16,10 @@ class ShoppingCart {
         this.items.push(item);
     }
 
+    remove(item: string) {
+        this.items = this.items.filter(x => x !== item)
+    }
+
     getItems(): string[] {
         return this.items;
     }
@@ -32,24 +36,26 @@ export function start() {
  * The main menu. Will show until the user exits
  */
 function showMainMenu(sc: ShoppingCart) {
-  while(true){ //run until we exit
-    console.log(`Welcome to the Grocery System!
+    while (true) { //run until we exit
+        console.log(`Welcome to the Grocery System!
   1. Add a product to the cart.
   2. Get Total.
-  3. Quit.`);
+  3. Remove a product from the cart.
+  4. Quit.`);
 
-    let response = readlineSync.question('> ')
-    if(response === '3' || response.slice(0,2).toLowerCase() === ':q'){
-      break; //stop looping, thus leaving method
-    }
+        let response = readlineSync.question('> ')
+        if (response === '4' || response.slice(0, 2).toLowerCase() === ':q') {
+            break; //stop looping, thus leaving method
+        }
 
-    switch(response) { //handle each response
-      case '1': addProductToCart(sc); break;
-      case '2': getTotalOfCart(sc); break;
-      default: console.log('Invalid option!');
+        switch (response) { //handle each response
+            case '1': addProductToCart(sc); break;
+            case '2': getTotalOfCart(sc); break;
+            case '3': removeProductFromCart(sc); break;
+            default: console.log('Invalid option!');
+        }
+        console.log(''); //extra empty line for revisiting
     }
-    console.log(''); //extra empty line for revisiting
-  }
 }
 
 function showProducts() {
@@ -65,15 +71,21 @@ function showProducts() {
 
 function addProductToCart(sc: ShoppingCart) {
     showProducts();
-    
+
     console.log(`Which product would you like to add?`);
     let response = readlineSync.question('> ')
     sc.add(response);
 }
 
+function removeProductFromCart(sc: ShoppingCart) {
+    for (let item in sc.items){
+        console.log(`-- Select ${sc.items[item]}: ${sc.items[item]}`)
+    }
+}
+
 function getTotalOfCart(sc: ShoppingCart) {
     let cartStr = JSON.stringify(sc.getItems());
-    console.log("Getting Total in Client: "+cartStr);
+    console.log("Getting Total in Client: " + cartStr);
 
     const total = fetch(url + "/total", {
         method: 'POST',
@@ -81,9 +93,21 @@ function getTotalOfCart(sc: ShoppingCart) {
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
-        }});
-
-    console.log(`Your cart total is $${total.json()}.`);
+        }
+    });
+    const taxes = fetch(url + "/taxes", {
+        method: 'POST',
+        body: cartStr,
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    });
+    let subtotalnum:number = total.json();
+    let taxesnum:number = taxes.json();
+    console.log(`Your cart subtotal is $${subtotalnum}.`);
+    console.log(`Your cart taxes is $${taxesnum}.`);
+    console.log(`Your cart total is $${subtotalnum+taxesnum}.`)
 }
 
 start();
--- Server/build/server.js
+++ Server/build/server.js
@@ -5,7 +5,8 @@ var bodyParser = require("body-parser");
 var productDatabase = {
     'apples': { 'title': 'Tasty Apples', 'price': 2.19 },
     'oranges': { 'title': 'Delicious Oranges', 'price': 2.88 },
-    'grapes': { 'title': 'Grapes. Just grapes.', 'price': 1.69 }
+    'grapes': { 'title': 'Grapes. Just grapes.', 'price': 1.69 },
+    'banna': { 'title': 'Banna are full of K', 'price': 1.12 }
 };
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +21,12 @@ app.post('/total', function (req, res) {
     console.log("Sending: " + total);
     res.send("" + total);
 });
+app.post('/taxes', function (req, res) {
+    var cart = req.body;
+    var taxes = getTaxes(cart);
+    console.log("Sending Taxes: " + taxes);
+    res.send("" + taxes);
+});
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
@@ -32,7 +39,16 @@ function getTotal(cart) {
         var item = cart_1[_i];
         total += getPrice(item);
     }
-    return total;
+    return Math.round(total*100)/100;
+}
+function getTaxes(cart) {
+    console.log("Get Taxes: " + cart);
+    var taxes = 0;
+    for (var _i = 0, cart_2 = cart; _i < cart_2.length; _i++) {
+        var item = cart_2[_i];
+        taxes += getPrice(item) * 0.095;
+    }
+    return Math.round(taxes * 100) / 100;
 }
 function getPrice(item) {
     console.log("GetPrice: " + item);
